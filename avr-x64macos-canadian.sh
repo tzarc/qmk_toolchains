1f9327a0f97fddc7d244cd98528718796380bc93
Nick Brassel <nick@tzarc.org>
2025-02-28 10:08:53 +1100

    Source tarballs.

---
+++ avr-x64macos-canadian.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Copyright 2024-2025 Nick Brassel (@tzarc)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+this_script="$PWD/$(basename ${BASH_SOURCE[0]})"
+script_dir=$(dirname "${this_script}")
+cd "$script_dir"
+source "${script_dir}/common.bashinc"
+
+build_one_help "$@"
+respawn_docker_if_needed "$@"
+
+build_one \
+    --sample-name=avr \
+    --canadian-host=x86_64-apple-darwin24 \
+    --binutils-plugins \
+    --no-cross-gdb-python \
+    "$@"
